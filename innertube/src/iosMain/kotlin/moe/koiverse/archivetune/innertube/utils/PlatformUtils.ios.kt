84d9b642f241214d5df4073bac038de318e5639b
koiverse <itzmerinz.dc@gmail.com>
2026-04-18 01:11:20 +0700

    refactor(PlatformUtils): update CC_SHA1 import to CoreCrypto and fix hash formatting

--- innertube/src/iosMain/kotlin/moe/koiverse/archivetune/innertube/utils/PlatformUtils.ios.kt
+++ innertube/src/iosMain/kotlin/moe/koiverse/archivetune/innertube/utils/PlatformUtils.ios.kt
@@ -5,8 +5,8 @@ package moe.koiverse.archivetune.innertube.utils
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.refTo
 import kotlinx.cinterop.usePinned
-import platform.CommonCrypto.CC_SHA1
-import platform.CommonCrypto.CC_SHA1_DIGEST_LENGTH
+import platform.CoreCrypto.CC_SHA1
+import platform.CoreCrypto.CC_SHA1_DIGEST_LENGTH
 import platform.Foundation.NSDate
 import platform.Foundation.NSLocale
 import platform.Foundation.countryCode
@@ -16,11 +16,11 @@ import platform.Foundation.timeIntervalSince1970
 
 actual fun sha1(str: String): String {
     val data = str.encodeToByteArray()
-    val hash = UByteArray(CC_SHA1_DIGEST_LENGTH)
+    val hash = UByteArray(CC_SHA1_DIGEST_LENGTH.toInt())
     data.usePinned { pinned ->
         CC_SHA1(pinned.addressOf(0), data.size.toUInt(), hash.refTo(0))
     }
-    return hash.joinToString("") { "%02x".format(it.toByte()) }
+    return hash.joinToString("") { (it.toInt() and 0xFF).toString(16).padStart(2, '0') }
 }
 
 actual fun currentTimeSeconds(): Long =
--- lastfm/src/iosMain/kotlin/moe/koiverse/archivetune/lastfm/utils/PlatformUtils.ios.kt
+++ lastfm/src/iosMain/kotlin/moe/koiverse/archivetune/lastfm/utils/PlatformUtils.ios.kt
@@ -5,14 +5,14 @@ package moe.koiverse.archivetune.lastfm.utils
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.refTo
 import kotlinx.cinterop.usePinned
-import platform.CommonCrypto.CC_MD5
-import platform.CommonCrypto.CC_MD5_DIGEST_LENGTH
+import platform.CoreCrypto.CC_MD5
+import platform.CoreCrypto.CC_MD5_DIGEST_LENGTH
 
 actual fun md5(str: String): String {
     val data = str.encodeToByteArray()
-    val hash = UByteArray(CC_MD5_DIGEST_LENGTH)
+    val hash = UByteArray(CC_MD5_DIGEST_LENGTH.toInt())
     data.usePinned { pinned ->
         CC_MD5(pinned.addressOf(0), data.size.toUInt(), hash.refTo(0))
     }
-    return hash.joinToString("") { "%02x".format(it.toByte()) }
+    return hash.joinToString("") { (it.toInt() and 0xFF).toString(16).padStart(2, '0') }
 }
